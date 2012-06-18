7c514fb0b0767958ffa56c0a07670987a4c27dfa
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-06-18 06:37:23 +0400

    small refactoring in thresholds_by_pvalues

--- lib/macroape/count_by_threshold.rb
+++ lib/macroape/count_by_threshold.rb
@@ -5,7 +5,7 @@ module Bioinform
     def counts_by_thresholds(*thresholds)
       scores = count_distribution_after_threshold(thresholds.min)
       thresholds.map{ |threshold|
-        scores.inject(0){|sum,(score,count)|  (score >= threshold) ? sum + count : sum}
+        scores.inject(0.0){|sum,(score,count)|  (score >= threshold) ? sum + count : sum}
       }
     end
 
--- lib/macroape/threshold_by_pvalue.rb
+++ lib/macroape/threshold_by_pvalue.rb
@@ -17,30 +17,28 @@ module Bioinform
       max_pvalue = pvalues.max
       max_look_for_count = max_pvalue * vocabulary_volume
       scores={}
-      until scores.inject(0){|sum,(score,count)| sum + count} >= max_look_for_count
+      until scores.inject(0.0){|sum,(score,count)| sum + count} >= max_look_for_count
         scores = count_distribution_after_threshold(threshold_gauss_estimation(max_pvalue))
         max_pvalue *=2 # if estimation counted too small amount of words - try to lower threshold estimation by doubling pvalue
       end
-      pvalue_counts = pvalues.sort.inject(Hash.new){|h, pvalue| h.merge pvalue => pvalue * vocabulary_volume }
+      
+      pvalue_counts = pvalues.sort.collect_hash{|pvalue| [pvalue, pvalue * vocabulary_volume] }
       look_for_counts = pvalue_counts.to_a
-      sum_count = 0
+      sum_count = 0.0
       scores = scores.sort.reverse
       results = {}
       scores.size.times do |i|
         while !look_for_counts.empty? and sum_count + scores[i][1] > look_for_counts.first[1] # usually this 'while' works as 'if'
-          cnt = look_for_counts.shift
-          pval = cnt[0]
-          score = cnt[1]
-          
-          threshold_2 = scores[i][0]
-          sum_count_2 = sum_count + scores[i][1]
-          if i>0
-            threshold = scores[i-1][0]
-            results[pval] = [threshold_2.to_f, sum_count_2, threshold.to_f, sum_count.to_f]
+          pval, score = look_for_counts.shift
+          threshold_2, sum_count_2  =  scores[i][0].to_f, (sum_count + scores[i][1]).to_f
+          if i > 0
+            threshold = scores[i-1][0].to_f
+            results[pval] = [threshold_2, sum_count_2, threshold, sum_count]
           else          
-            results[pval] = [threshold_2.to_f, sum_count_2.to_f, best_score + 1.0, 0.0]
+            results[pval] = [threshold_2, sum_count_2, best_score + 1.0, 0.0]
           end
         end
+        
         sum_count += scores[i][1]
       end
       results
