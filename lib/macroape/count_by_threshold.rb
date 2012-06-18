6b385a61463ac4f16e71acdbc25d509201d0f011
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-06-18 06:07:38 +0400

    small typo fixes

--- lib/macroape/count_by_threshold.rb
+++ lib/macroape/count_by_threshold.rb
@@ -4,28 +4,10 @@ module Bioinform
   class PWM
     def counts_by_thresholds(*thresholds)
       scores = count_distribution_after_threshold(thresholds.min)
-=begin
+
        thresholds.map{ |threshold|
         scores.inject(0){|sum,(score,count)|  (score >= threshold) ? sum + count : sum}
       }
-=end
-      s_thr= thresholds.map.with_index{|threshold,index|[threshold,index]}.sort_by{|threshold,index| threshold}
-      
-      cnt = 0
-      thr_cnts=[]
-      
-      scores.sort.reverse.each do |score,count|
-        while !s_thr.empty? and score < s_thr.last[0]
-          thr_cnts.push(cnt)
-          s_thr.pop
-        end
-        cnt += count
-      end
-      s_thr = thresholds.map.with_index{|threshold,index|[threshold,index]}.sort_by{|threshold,index| threshold}
-      while thr_cnts.size < s_thr.size
-        thr_cnts.push(cnt)
-      end
-      s_thr.reverse.zip(thr_cnts).sort_by{|(threshold,index), count| index}.map{|(threshold,index), count| count.to_f}
     end
 
     def pvalue_by_threshold(threshold)
