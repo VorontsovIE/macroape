ea9263ae73d54d7a4ad110d95299b7ab6f659bed
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-06-21 12:43:11 +0400

    tests changed not to be broken with unix newline style

--- test/preprocess_collection_test.rb
+++ test/preprocess_collection_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class TestPreprocessCollection < Test::Unit::TestCase
   def test_multipvalue_preproceessing
-    system(Helpers.exec_cmd('preprocess_collection','./test/data/test_collection -o test/data/test_collection.yaml.tmp -p 0.0005 0.0001 0.00005 --silent'))
-    assert_equal File.read('test/data/test_collection.yaml'), File.read('test/data/test_collection.yaml.tmp')
+    system(Helpers.exec_cmd('preprocess_collection','test/data/test_collection -o test/data/test_collection.yaml.tmp -p 0.0005 0.0001 0.00005 --silent'))
+    assert_equal File.read('test/data/test_collection.yaml').gsub("\r\n","\n"), File.read('test/data/test_collection.yaml.tmp').gsub("\r\n", "\n")
     File.delete 'test/data/test_collection.yaml.tmp'
   end
 end
--- test/scan_collection_test.rb
+++ test/scan_collection_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class TestScanCollection < Test::Unit::TestCase
   def test_scan_default_cutoff
-    assert_equal File.read('test/data/KLF4_f2_scan_results_default_cutoff.txt'), 
-                 IO.popen(Helpers.exec_cmd('scan_collection','test/data/KLF4_f2.pat test/data/test_collection.yaml --silent'), &:read)
+    assert_equal File.read('test/data/KLF4_f2_scan_results_default_cutoff.txt').gsub("\r\n", "\n"),
+                 IO.popen(Helpers.exec_cmd('scan_collection','test/data/KLF4_f2.pat test/data/test_collection.yaml --silent'), &:read).gsub("\r\n","\n")
   end
   def test_scan_and_output_all_results
-    assert_equal File.read('test/data/KLF4_f2_scan_results_all.txt'), 
-                 IO.popen(Helpers.exec_cmd('scan_collection','test/data/KLF4_f2.pat test/data/test_collection.yaml --all --silent'), &:read)
+    assert_equal File.read('test/data/KLF4_f2_scan_results_all.txt').gsub("\r\n", "\n"),
+                 IO.popen(Helpers.exec_cmd('scan_collection','test/data/KLF4_f2.pat test/data/test_collection.yaml --all --silent'), &:read).gsub("\r\n","\n")
       
   end
   def test_scan_precise_mode
-    assert_equal File.read('test/data/KLF4_f2_scan_results_precise_mode.txt'),
-                 IO.popen(Helpers.exec_cmd('scan_collection','test/data/KLF4_f2.pat test/data/test_collection.yaml --precise --all --silent'), &:read)
+    assert_equal File.read('test/data/KLF4_f2_scan_results_precise_mode.txt').gsub("\r\n","\n"),
+                 IO.popen(Helpers.exec_cmd('scan_collection','test/data/KLF4_f2.pat test/data/test_collection.yaml --precise --all --silent'), &:read).gsub("\r\n", "\n")
   end
 end
