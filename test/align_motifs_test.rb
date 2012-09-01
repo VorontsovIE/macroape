367b13e45a59eebe1498d1c0811dee3075af9338
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-09-01 15:26:25 +0400

    align_motifs added spec for pcm input format

--- test/align_motifs_test.rb
+++ test/align_motifs_test.rb
@@ -5,4 +5,8 @@ class TestAlignmotifs < Test::Unit::TestCase
     assert_equal "test/data/KLF4_f2.pat\t0\tdirect\ntest/data/KLF3_f1.pat\t-4\tdirect\ntest/data/SP1_f1_revcomp.pat\t-1\trevcomp\n", 
       Helpers.align_motifs_output('test/data/KLF4_f2.pat  test/data/KLF3_f1.pat  test/data/SP1_f1_revcomp.pat')
   end
+  def test_align_pcm_motifs
+    assert_equal "test/data/KLF4_f2.pcm\t0\tdirect\ntest/data/KLF3_f1.pcm\t-4\tdirect\ntest/data/SP1_f1_revcomp.pcm\t-1\trevcomp\n", 
+      Helpers.align_motifs_output('--pcm test/data/KLF4_f2.pcm  test/data/KLF3_f1.pcm  test/data/SP1_f1_revcomp.pcm')
+  end
 end
