efc979d1ebaa1c13edcdfa65e60b0d33862a7820
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-09-14 19:47:32 +0400

    unused (commented) methods removed

--- lib/macroape/pwm_compare_aligned.rb
+++ lib/macroape/pwm_compare_aligned.rb
@@ -89,26 +89,6 @@ module Macroape
       end
     end
 
-=begin
-    def discrete(rate)
-      PWMCompareAligned.new(first.discrete(rate), second.discrete(rate))
-    end
-
-    def sort_pair_of_matrices_by(&block)
-      mat = first.pwm.zip(second.pwm).sort_by(&block).transpose
-      PWMCompareAligned.new(SinglePWM(mat[0],first.probabilities), SinglePWM(mat[1], second.probabilities))
-    end
-    def sort_decreasing_max
-      PWMCompareAligned.new(*sort_pair_of_matrices_by{|col_pair| -col_pair[0].max} )
-    end
-    def sort_increasing_min
-      PWMCompareAligned.new(*sort_pair_of_matrices_by{|col_pair| col_pair[0].min} )
-    end
-    def permute_columns(permutation_index)
-      PWMCompareAligned.new(first.permute(permutation_index), second.permute(permutation_index))
-    end
-=end
-
     def jaccard(first_threshold, second_threshold)
       f = first.counts_by_thresholds(first_threshold).first
       s = second.counts_by_thresholds(second_threshold).first
