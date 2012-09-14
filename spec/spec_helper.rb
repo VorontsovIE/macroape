d7af21eadb6abacd9592c92dfc3af5123a02195e
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-09-14 18:36:07 +0400

    fix for bioinform-require path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-$bioinform_folder = File.dirname(__FILE__) + '/../../bioinform/lib'
+$bioinform_folder = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'bioinform', 'lib'))
 $LOAD_PATH.unshift $bioinform_folder
 
 require 'rspec'
